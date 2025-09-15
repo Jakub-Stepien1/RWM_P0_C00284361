e19baae796ecff2c1d2406a79a852ea5d2989e87
Jakub Stepien <ninjadeluxepl@gmail.com>
2025-09-15 10:59:37 +0100

    init sveltekit + vitest; add cube filter + tests

---
+++ src/lib/filters/index.ts
@@ -0,0 +1,4 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+export * from './cube';
+// Later, export your assigned filter here too, e.g. export * from './modulo3';
